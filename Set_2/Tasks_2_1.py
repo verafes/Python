4a72694b4fc0be2b7f4abe7b9560f05b4e822ef3
verafes@gmail.com <verafes@gmail.com>
2020-09-26 23:41:10 -0700

    Update Tasks_2_1.py
    A new task and solution is added to set

--- Set_2/Tasks_2_1.py
+++ Set_2/Tasks_2_1.py
@@ -50,8 +50,29 @@ https://www.codewars.com/kata/58fa273ca6d84c158e000052
 def digits(n):
     return len(str(n))
 
-# Task 5. 
-	
+# Task 5. Basic Mathematical Operations
+# Your task is to create a function that does four basic mathematical operations.
+# The function should take three arguments - operation(string/char), value1(number), value2(number).
+# The function should return result of numbers after applying the chosen operation.
+# Examples
+# basic_op('+', 4, 7)         # Output: 11
+# basic_op('-', 15, 18)       # Output: -3
+# basic_op('*', 5, 5)         # Output: 25
+# basic_op('/', 49, 7)        # Output: 7
+
+def basic_op(operator, value1, value2):
+    if operator == "+":
+        return value1 + value2
+    elif operator == "-":
+        return value1 - value2
+    elif operator == "*":
+        return value1 * value2
+    elif operator == "/":
+        return value1 / value2
+    else:
+        return "Error"
+
+
 # Task 6.
 	
 # Task 7. 
