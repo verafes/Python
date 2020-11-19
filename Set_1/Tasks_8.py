b300f2f1936ffac2a00713ff57259f39829b459b
verafes <verafes@gmail.com>
2020-11-18 21:09:21 -0800

    Solution Set 1 updated
    updated set 1-8

--- Set_1/Tasks_8.py
+++ Set_1/Tasks_8.py
@@ -112,11 +112,20 @@ def is_divisible(n,x,y):
 
 # Task 78. ATM
 https://www.codewars.com/kata/5635e7cb49adc7b54500001c
-
 # There is enough money available on ATM in nominal value 10, 20, 50, 100, 200 and 500 dollars.
 # You are given money in nominal value of n with 1<=n<=1500.
 # Try to find minimal number of notes that must be used to repay in dollars, or output -1 if it is impossible.
 
+def solve(n):
+    count=0
+    y = [500,200,100,50,20,10]
+    i = 0
+    if n % 10 != 0: return -1
+    while n > 0:
+        count=count + (n//y[i])
+        n = int(n%y[i])
+        i = i + 1
+    return count
 
 # Task 79. Sum of Digits / Digital Root
 https://www.codewars.com/kata/541c8630095125aba6000c00
@@ -141,20 +150,13 @@ def digital_root(n):
 def digital_root(n):
     return 1 + ((int(n) - 1) % 9) if n>0 else 0
 	
-# Task 70. ATM
-https://www.codewars.com/kata/5635e7cb49adc7b54500001c
-# There is enough money available on ATM in nominal value 10, 20, 50, 100, 200 and 500 dollars.
-# You are given money in nominal value of n with 1<=n<=1500.
-# Try to find minimal number of notes that must be used to repay in dollars, or output -1 if it is impossible.
+# Task 80. Jenny's secret message
+https://www.codewars.com/kata/55225023e1be1ec8bc000390
+# Jenny has written a function that returns a greeting for a user. 
+# However, she's in love with Johnny, and would like to greet him slightly different. 
+# She added a special case to her function, but she made a mistake.
+# Can you help her?
 
-def solve(n):
-    count=0
-    y = [500,200,100,50,20,10]
-    i = 0
-    if n % 10 != 0: return -1
-    while n > 0:
-        count=count + (n//y[i])
-        n = int(n%y[i])
-        i = i + 1
-    return count
-
+def greet(name):
+    return "Hello, my love!" if name == "Johnny" else f"Hello, {name}!"
+#
